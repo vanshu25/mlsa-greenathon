d0799506a16adc10340113ea0ed71ecc09257c2a
Gino Messmer <gino@messmerized.de>
2021-04-19 00:37:25 +0200

    Add models and query function

---
+++ api/MlsaGreenathon.Models/Business.cs
@@ -0,0 +1,11 @@
+﻿namespace MlsaGreenathon.Models
+{
+    public class Business : Entity
+    {
+        public string Name { get; set; }
+
+        public bool IsApproved { get; set; }
+
+        public override string PartitionKey { get; set; } = "Business";
+    }
+}
---
+++ api/MlsaGreenathon.Models/Entity.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Microsoft.Azure.Cosmos.Table;
+
+namespace MlsaGreenathon.Models
+{
+    public abstract class Entity : TableEntity
+    {
+        public string Id => RowKey;
+
+        public new abstract string PartitionKey { get; set; }
+
+        protected Entity()
+        {
+            RowKey = Guid.NewGuid().ToString();
+        }
+    }
+}
