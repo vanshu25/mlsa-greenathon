9bf2a90d836ddbadf3c41d04d17a80cd2ab7d06b
Gino Messmer <git@ginomessmer.me>
2021-04-22 23:19:41 +0200

    Add Azure Maps

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,65 @@
+
+import {AzureMap, AzureMapsProvider, IAzureMapOptions} from 'react-azure-maps'
+import {AuthenticationType} from 'azure-maps-control'
 import './App.scss';
+
+const azureMapOptions = {
+  authOptions: {
+      authType: AuthenticationType.subscriptionKey,
+      subscriptionKey: 'Ea_uzBbOhHmVPM5UjV-vEoW7wRw0bO-RTc7bo7AhtIw'
+  },
+}
 
 const App = () => {
   return (
     <div className="App">
-      <div className="sidebar open">
-        <div className="heading">
-          <span className="label">Sort and filter</span>
-        </div>
-
+      <AzureMapsProvider>
         <div>
-          <div className="input">
-            <label htmlFor="sortBy" className="label">Sort by</label>
-            <select name="sortBy" id="sortBy"></select>
+          <div className="map-container">
+            <AzureMap options={azureMapOptions} />
           </div>
-          <div className="input">
-            <label htmlFor="distance" className="label">Distance</label>
-            <input id="distance" type="text"/>
+
+          <div className="sidebar open">
+            <div className="heading">
+              <span className="label">Sort and filter</span>
+            </div>
+
+            <div>
+              <div className="input">
+                <label htmlFor="sortBy" className="label">Sort by</label>
+                <select name="sortBy" id="sortBy"></select>
+              </div>
+              <div className="input">
+                <label htmlFor="distance" className="label">Distance</label>
+                <input id="distance" type="text"/>
+              </div>
+              <div className="input">
+                <label htmlFor="sustainability" className="label">Scale of sustainability</label>
+                <input id="sustainability" type="text"/>
+              </div>
+              <div className="input">
+                <label htmlFor="industry" className="label">Industry</label>
+                <input id="industry" type="text"/>
+              </div>
+              <div className="input">
+                <label htmlFor="type" className="label">Type</label>
+                <div className="button-group">
+                  <button className="active">Non-Profit</button>
+                  <button>Profit</button>
+                </div>
+              </div>
+            </div>
           </div>
-          <div className="input">
-            <label htmlFor="sustainability" className="label">Scale of sustainability</label>
-            <input id="sustainability" type="text"/>
+
+          <div className="search-box">
+            <input type="text" name="searchBox" id="searchBox" placeholder="Search a business..."/>
           </div>
-          <div className="input">
-            <label htmlFor="industry" className="label">Industry</label>
-            <input id="industry" type="text"/>
-          </div>
-          <div className="input">
-            <label htmlFor="type" className="label">Type</label>
-            <input id="type" type="text"/>
+        
+          <div className="fab-overlay">
+            <button className="fab">Add a new business</button>
           </div>
         </div>
-      </div>
-
-      <div className="search-box">
-        <input type="text" name="searchBox" id="searchBox" placeholder="Search a business..."/>
-      </div>
-    
-      <div className="fab-overlay">
-        <button className="fab">Add a new business</button>
-      </div>
+      </AzureMapsProvider>
 
     </div>
   );
