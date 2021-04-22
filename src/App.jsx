a4a5ff87799e424e8c5cf8c49d944da25186c839
Gino Messmer <git@ginomessmer.me>
2021-04-22 22:43:17 +0200

    Add sidebar

---
+++ src/App.jsx
@@ -0,0 +1,39 @@
+import './App.scss';
+
+const App = () => {
+  return (
+    <div className="App">
+      <div className="sidebar">
+        <div className="heading">
+          <span className="label">Sort and filter</span>
+        </div>
+
+        <div>
+          <div className="input">
+            <label htmlFor="sortBy" className="label">Sort by</label>
+            <select name="sortBy" id="sortBy"></select>
+          </div>
+          <div className="input">
+            <label htmlFor="distance" className="label">Distance</label>
+            <input id="distance" type="text"/>
+          </div>
+          <div className="input">
+            <label htmlFor="sustainability" className="label">Scale of sustainability</label>
+            <input id="sustainability" type="text"/>
+          </div>
+          <div className="input">
+            <label htmlFor="industry" className="label">Industry</label>
+            <input id="industry" type="text"/>
+          </div>
+          <div className="input">
+            <label htmlFor="type" className="label">Type</label>
+            <input id="type" type="text"/>
+          </div>
+        </div>
+      </div>
+    
+    </div>
+  );
+}
+
+export default App;
