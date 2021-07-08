0b6450a543caaf5f806bf36b588e207c66f99e65
Vanshika <vanshikanamdev.nakur@gmail.com>
2021-07-08 12:12:00 +0530

    added the autocomplete feature for search box

--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -1,13 +1,28 @@
 import React from 'react';
-
+import JSONDATA from "./MOCK_DATA.json"
 import './SearchBox.scss';
+import {useState} from 'react';
+function SearchBox() {
+const [searchTerm, setSearchTerm] = useState('')
 
-const SearchBox = () => {
   return (
     <div className="search-box">
-      <input type="text" name="searchBox" id="searchBox" placeholder="Search a business..."/>
-    </div>
+      <input type="text" name="searchBox" id="searchBox" placeholder="Search a business..." onChange={event => {setSearchTerm(event.target.value)}}/>
+      {JSONDATA.filter((val)=> {
+        if (searchTerm == ""){
+          return val
+        } else if (val.title.toLowerCase().includes(searchTerm.toLowerCase())){
+          return val
+        }
+      }).map((val,key)=>{
+        return(
+         <div className="user" key={key}>
+         <p>{val.title}</p>
+         </div>
+       );
+      })}
+      </div>
   );
-};
+}
 
 export default SearchBox;
